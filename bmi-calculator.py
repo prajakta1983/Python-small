ce944db11ae0867a17939d2db51bd325d2db49df
prajakta1983 <72853630+prajakta1983@users.noreply.github.com>
2023-12-15 12:43:45 +0530

    Add files via upload

--- bmi-calculator.py
+++ bmi-calculator.py
@@ -5,9 +5,12 @@ bmi= weight / ((height)*(height))
 print("Your BMI is: " , bmi)
 if(bmi<18.5):
     print("You are underweight")
+    print("You need to increase weight by " + str(18.5-bmi) + " to be in normal range")
 elif(18.5<bmi<24.9):
     print("You are normal")
 elif(25<bmi<29.9):
     print("You are overweight")
+    print("You need to reduce weight by " + str(bmi-24.9) + " to be in normal range")
 elif(bmi>30):
     print("You are obese")
+    print("You need to increase weight by " + str(bmi-24.9) + " to be in normal range")
